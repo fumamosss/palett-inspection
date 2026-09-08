952588156f392448d17e4b20f0ea419c6dbe1ba7
fumamosss <fumamosss@icloud.com>
2026-09-08 10:24:59 +0500

    refactor(dist_test): self-suffient DistanceBlock, drop worker from screen

--- screens/dist_test_screen.py
+++ screens/dist_test_screen.py
@@ -4,7 +4,6 @@ from textual.screen import Screen
 from textual.widgets import Footer
 
 from service_panel import DistanceBlock
-from screens import workers
 
 
 class DistTestScreen(Screen):
@@ -13,22 +12,6 @@ class DistTestScreen(Screen):
     def compose(self):
         yield DistanceBlock()
         yield Footer()
-
-    def on_mount(self):
-        self.stopped = False
-        block = self.query_one(DistanceBlock)
-
-        def on_update(dist, state, photos):
-            if dist is not None:
-                block.set_distance(dist)
-
-        self.run_worker(
-            lambda: workers.distance_loop(on_update, lambda: self.stopped),
-            thread=True,
-        )
-
-    def on_unmount(self):
-        self.stopped = True
 
     def action_back(self):
         self.app.pop_screen()
